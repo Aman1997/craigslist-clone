ccba10f4fc9c3c9ceaf999b12908bd3635da4ba0
Aman Bisht <amanb218@gmail.com>
2020-05-17 22:47:42 +0530

    Minor UI update

--- myapp/views.py
+++ myapp/views.py
@@ -16,23 +16,28 @@ def search(request):
     soup = BeautifulSoup(response.text, features='html.parser')
 
     post_listings = soup.find_all('li',{'class':'result-row'})
+    if(post_listings):
+        final_postings = []
+        for post in post_listings:
+            post_title = post.find(class_='result-title').text
+            post_url = post.find('a').get('href')
 
-    final_postings = []
-    for post in post_listings:
-        post_title = post.find(class_='result-title').text
-        post_url = post.find('a').get('href')
+            if (post.find(class_ = 'result-image').get('data-ids')):
+                post_image = post.find(class_ = 'result-image').get('data-ids').split(',')[0].split(':')[1]
+                post_image_url = CRAIGSLIST_IMG_URL.format(post_image)
+            else:
+                post_image_url = "https://craigslist.org/images/peace.jpg"    
 
-        if (post.find(class_ = 'result-image').get('data-ids')):
-            post_image = post.find(class_ = 'result-image').get('data-ids').split(',')[0].split(':')[1]
-            post_image_url = CRAIGSLIST_IMG_URL.format(post_image)
-        else:
-            post_image_url = "https://craigslist.org/images/peace.jpg"    
+            final_postings.append((post_title,post_url,post_image_url))
 
-        final_postings.append((post_title,post_url,post_image_url))
-
-    item_values = {
-        "search_content" : request.POST.get('search'),
-        "final_postings" : final_postings,
-    }
-
-    return render(request, 'myapp/search.html', item_values)    
+        item_values = {
+            "search_content" : request.POST.get('search'),
+            "final_postings" : final_postings,
+            "result" : True,
+        }
+  
+    else:
+        item_values = {
+            "result": False
+        }
+    return render(request, 'myapp/search.html', item_values)         
