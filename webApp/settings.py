d394f8047e6043df7f766ac1135511187d27d189
Aman Bisht <amanb218@gmail.com>
2020-07-28 21:20:49 +0530

    Update settings.py

--- webApp/settings.py
+++ webApp/settings.py
@@ -25,9 +25,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = '-%(8&b(vcyw-7-qy_x@#qtpwx&#6u4716ej#4nxp2o#h%jxq33'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
